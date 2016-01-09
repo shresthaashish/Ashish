ae3dadceb9cab8a874fc876a8a781defea70117a
Ashish Shrestha <ashish@planet.com>
2016-01-08 16:54:37 -0800

    anagram added

---
+++ anagram_ashish.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+'''This script  find all of the anagrams in a dictionary
+ in which there are at least 4 letters in the word and
+ atleast as many anagrams as there are letters.'''
+
+import sys
+from collections import defaultdict
+
+
+def load_dictionary(dict_location):
+    try:
+        with open(dict_location) as w:
+            for word_list in w:
+                yield word_list.rstrip()
+    except:
+        print("file does not exist")
+        sys.exit(0)
+
+
+def find_anagram(dictionary):
+    data = defaultdict(list)
+    for word in dictionary:
+        key = "".join(sorted(word))
+        if len(key) >= 4:
+            data[key].append(word)
+    for key, anagram in data.items():
+        if len(anagram) >= len(key):
+            print ", ".join(anagram)
+
+
+def main():
+	dict_location = '/usr/share/dict/words'
+	dictionary = load_dictionary(dict_location)
+	find_anagram(dictionary)
+
+
+if __name__ == "__main__":
+	main()
